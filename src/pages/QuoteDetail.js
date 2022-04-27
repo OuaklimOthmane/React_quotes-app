3860dc79db69b7e45526172bfc093dcb1f67e955
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-27 21:37:23 +0000

    Dislaying the quote Detail

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const QUOTES = [
+  { id: "q1", author: "othmane", text: "react is good" },
+  { id: "q2", author: "ouaklim", text: "react is great" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
   const { quoteId } = params;
 
+  //* Get the quote matched with the params's id :
+  const quote = QUOTES.find((quote) => quote.id === quoteId);
+
   return (
     <>
-      <h1>QuoteDetail</h1>
-      <div>{quoteId}</div>
-
+      <HighlightedQuote text={quote.text} author={quote.author} />
       {/* <Route path={`/quotes/:id/comments`} exact> */}
       <Route path={`/quotes/${quoteId}/comments`} exact>
         <Comments />
