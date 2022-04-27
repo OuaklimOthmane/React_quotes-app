772513bd4fc7beb2df3ca582ec94cdb1793300fa
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-27 21:11:32 +0000

    display some dummy quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,13 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const QUOTES = [
+  { id: "q1", author: "othmane", text: "react is good" },
+  { id: "q2", author: "ouaklim", text: "react is great" },
+];
 
 const AllQuotes = () => {
-  return <div>AllQuotes</div>;
+  return <QuoteList quotes={QUOTES} />;
 };
 
 export default AllQuotes;
