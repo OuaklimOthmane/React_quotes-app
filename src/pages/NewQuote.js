03f9e34477f97b228f16a62e033826860369e2d7
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-30 01:44:20 +0000

    implementing programming navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = (props) => {
+  const history = useHistory();
+
   const AddQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    //* Redirct to the quotes page :
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={AddQuoteHandler} />;
 };
 
 export default NewQuote;
+
+//! useHistory :
+//? The "useHistory" hook gives you access to the history instance that you may use to navigate
+//? "push(path, [state])": Pushes a new entry onto the history stack. Useful to redirect users to page
+//? "replace(path, [state])": Replaces the current entry on the history stack
