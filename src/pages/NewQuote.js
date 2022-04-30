4b7d7654229363207b614f2287b9a0f24fee663c
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-30 02:09:27 +0000

    Preventing possibly unwanted route transition with the Prompt component

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -18,6 +18,8 @@ const NewQuote = (props) => {
 export default NewQuote;
 
 //! useHistory :
-//? The "useHistory" hook gives you access to the history instance that you may use to navigate
-//? "push(path, [state])": Pushes a new entry onto the history stack. Useful to redirect users to page
-//? "replace(path, [state])": Replaces the current entry on the history stack
+//? The "useHistory" hook gives you access to the history instance that you may use to navigate.
+//? "push(path, [state])": Pushes a new entry onto the history stack. Useful to redirect users to page.
+//? "replace(path, [state])": Replaces the current entry on the history stack.
+
+//* In this case we progammed the navigation cause we need to use a button instead of using a <Link/> component, because we have to submit the form and also redircting to the quotes page, that's why we programmed the navigation imperatively.
