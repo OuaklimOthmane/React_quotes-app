f6ccf5811ef4b74be47f1ae21b8693f159ee72fd
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-05 23:07:35 +0000

    dealing with status

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -17,6 +17,22 @@ const AllQuotes = () => {
     sendRequest();
   }, [sendRequest]);
 
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
   return <QuoteList quotes={loadedQuotes} />;
 };
 
