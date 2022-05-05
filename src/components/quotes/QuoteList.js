aca853f30b39db986bda294639eff68977a9339a
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-05 17:57:49 +0000

    Sorting quotes using query parameters

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,47 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  //! Extract the query params :
+  const queryParams = new URLSearchParams(location.search); // "location.search" returns a string containing all the query parameters.
+  // "URLSearchParams" API so that you can extract the query params and their values
+
+  //! Define the query value :
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  //! Sorting the quotes :
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  //! Define a query :
+  const changeSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "descending" : "ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
@@ -21,3 +55,12 @@ const QuoteList = (props) => {
 };
 
 export default QuoteList;
+
+//! useLocation() hook:
+//? The "useLocation" hook is a function that returns the location object that contains information about the current URL. this is like a state that always returns your current URL. Whenever the URL changes, a new location object will be returned.
+//? Location object properties:
+// "hash" : the anchor part (#)
+// "pathname" : the path name
+// "search" : the query string part
+// "state"
+// "key"
