f1eb587620bd80cf40305798a02f262240e199f0
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-05 22:54:34 +0000

    getAllQuotes function added

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,29 @@
 const FIREBASE_DOMAIN =
   "https://react-quotes-app-9e84d-default-rtdb.firebaseio.com";
 
+//! Getting all quotes :
+export async function getAllQuotes() {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Could not fetch quotes.");
+  }
+
+  const transformedQuotes = [];
+
+  for (const key in data) {
+    const quoteObj = {
+      id: key,
+      ...data[key],
+    };
+
+    transformedQuotes.push(quoteObj);
+  }
+
+  return transformedQuotes;
+}
+
 //! Adding a quote :
 export async function addQuote(quoteData) {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`, {
