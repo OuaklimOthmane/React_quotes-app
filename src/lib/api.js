5a6bf25c10269ddd56918e3764aa8296579a2fa6
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-05 22:43:34 +0000

    AddQuote function added

---
+++ src/lib/api.js
@@ -0,0 +1,20 @@
+const FIREBASE_DOMAIN =
+  "https://react-quotes-app-9e84d-default-rtdb.firebaseio.com";
+
+//! Adding a quote :
+export async function addQuote(quoteData) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`, {
+    method: "POST",
+    body: JSON.stringify(quoteData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Could not create quote.");
+  }
+
+  return null;
+}
