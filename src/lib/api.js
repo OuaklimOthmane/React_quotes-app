980aeb8ac4137205653ff70eeac3002594592133
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-06 16:32:32 +0000

    addComment function added

--- src/lib/api.js
+++ src/lib/api.js
@@ -58,3 +58,21 @@ export async function addQuote(quoteData) {
 
   return null;
 }
+
+//! Adding a comment :
+export async function addComment(commentData, quoteId) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/comments/${quoteId}.json`, {
+    method: "POST",
+    body: JSON.stringify(commentData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Could not add comment.");
+  }
+
+  return { commentId: data.name };
+}
