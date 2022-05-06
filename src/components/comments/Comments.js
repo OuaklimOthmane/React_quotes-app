7e8ab3b5782a02578ff19f8c57f46ac20ceb3b73
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-05-06 22:53:23 +0000

    propagate props to NewCommentForm

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,24 +1,33 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const addedCommentHandler = () => {};
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
+      {isAddingComment && (
+        <NewCommentForm
+          onAddedComment={addedCommentHandler}
+          quoteId={params.quoteId}
+        />
+      )}
       <p>Comments...</p>
     </section>
   );
